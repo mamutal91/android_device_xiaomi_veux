107c5cd4a8a145875431075b5c9f512886e18dd7
Alexandre Rangel <mamutal91@gmail.com>
2023-03-07 12:52:46 -0300

    veux: BringUp Evolution
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- evolution_veux.mk
+++ evolution_veux.mk
@@ -18,9 +18,9 @@ TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common PixelExperience
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := evolution_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -32,4 +32,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="veux-user 12 SKQ1.211006.001 V13.0.5.0.SKCMIXM release-keys"
 
 BUILD_FINGERPRINT := POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.5.0.SKCMIXM:user/release-keys
-
